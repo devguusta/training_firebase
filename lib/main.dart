b6479c97a31089ba6bc7429b684e7e6a8cefc856
Gustavo Silva <gustavocsil60@gmail.com>
2021-08-01 15:53:55 -0300

    cria método login e register no auth controller

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,8 @@
 import 'package:firebase1/firebase_app.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-import 'shared/auth/auth_controller.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(MultiProvider(
-    providers: [
-      ChangeNotifierProvider(create: (context) => AuthService())
-    ],
-    child: FirebaseWidget(),
-  ),
-  );
+  runApp(FirebaseWidget());
 }
 
